e55d73749758074dab32cce1ca8331073307fa58
Diego Bahena <diegoeblopez@outlook.com>
2026-02-10 13:47:55 -0600

    Added Integration Testing project and Login Test

---
+++ PetRadar.Web.API.IntegrationTests/DataSeeds/DefaultDataSet.cs
@@ -0,0 +1,56 @@
+﻿using PetRadar.Core.Data;
+using PetRadar.Core.Data.Entities;
+using PetRadar.Core.Data.Entities.Enums;
+using PetRadar.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetRadar.Web.API.IntegrationTests.DataSeeds
+{
+    public class DefaultDataSet : IDefaultDataSet
+    {
+
+        public static readonly int DefaultUserId = 1000;
+        public static readonly string DefaultUserEmail = "test@email.com";
+        public static readonly string DefaultUserPassword = "password";
+        public static readonly string DefaultUserName = "User";
+        public static readonly string DefaultUserLastName = "One";
+        public static readonly string DefaultUserPhoneNumber = "1234567890";
+        public static readonly RoleEnum DefaultUserRole = RoleEnum.Admin;
+
+
+        public UserEntity DefaultUserEntity { get; private set; }
+
+        public UserEntity DefaultUser { get => DefaultUserEntity; }
+
+        public DefaultDataSet()
+        {
+            DefaultUserEntity = new UserEntity();
+        }
+
+        public void SeedData(PetRadarDbContext dbContext)
+        {
+
+            var salt = UserDomain.GenerateSalt();
+            DefaultUserEntity = new UserEntity()
+            {
+                Id = DefaultUserId,
+                Email = DefaultUserEmail,
+                Salt = salt,
+                Password = UserDomain.GenerateHash(DefaultUserPassword, salt),
+                Name = DefaultUserName,
+                LastName = DefaultUserLastName,
+                PhoneNumber = DefaultUserPhoneNumber,
+                Role = DefaultUserRole,
+                IsActive = true
+            };
+            
+
+            dbContext.Users.Add(DefaultUserEntity);
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ PetRadar.Web.API.IntegrationTests/Helpers/PriorityOrderer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PetRadar.Web.API.IntegrationTests.Helpers
+{
+    public class PriorityOrderer : ITestCaseOrderer
+    {
+        public const string PriorityOrdererName = "SK.Poll.WebApi.IntegrationTests.Helpers.PriorityOrderer";
+        public const string PriorityOrdererAssemblyName = "SK.Poll.WebApi.IntegrationTests";
+
+        public IEnumerable<TTestCase> OrderTestCases<TTestCase>(IEnumerable<TTestCase> testCases) where TTestCase : ITestCase
+        {
+            var sortedMethods = new SortedDictionary<int, List<TTestCase>>();
+
+            foreach (TTestCase testCase in testCases)
+            {
+                int priority = 0;
+                foreach (var attr in testCase.TestMethod.Method.GetCustomAttributes(typeof(TestPriority).AssemblyQualifiedName))
+                    priority = attr.GetNamedArgument<int>(nameof(TestPriority.Priority));
+
+                GetOrCreate(sortedMethods, priority).Add(testCase);
+            }
+
+            foreach (var item in sortedMethods)
+            {
+                foreach (var testCase in item.Value)
+                    yield return testCase;
+            }
+        }
+
+        static TValue GetOrCreate<TKey, TValue>(IDictionary<TKey, TValue> dictionary, TKey key) where TValue : new()
+        {
+            if (dictionary.TryGetValue(key, out TValue result)) return result;
+
+            result = new TValue();
+            dictionary[key] = result;
+
+            return result;
+        }
+    }
+}
