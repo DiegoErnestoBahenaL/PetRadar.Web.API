86f746cffe0c7130b9949bb4d32c13ba2c22742a
Diego Bahena <diegoeblopez@outlook.com>
2026-03-04 15:44:35 -0600

    Added data annotation to CoreOptions

--- PetRadar.Core/PetRadarCoreOptions.cs
+++ PetRadar.Core/PetRadarCoreOptions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,9 @@ namespace PetRadar.Core
 {
     public class PetRadarCoreOptions
     {
+        [Required, MinLength(1)]
         public string MailGunAPIKey { get; set; }
-
+        [Required, MinLength(1)]
         public string BaseURL { get; set; }
     }
 }
